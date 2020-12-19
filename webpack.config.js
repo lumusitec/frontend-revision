24352d1a980a352af14720756e4302a0e4496c6e
lumusitec <lumusitec@gmail.com>
2020-12-19 19:46:24 -0300

    initial config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+
+const htmlWebpack = new HtmlWebpackPlugin({
+    template: './assets/index.template.html',
+    filename: 'index.html'
+});
 
 module.exports = {
     entry: './assets/javascript/entry.js',
@@ -7,6 +13,16 @@ module.exports = {
         path: path.join(__dirname, '..'),
         filename: 'dist/javascript/bundler.js'
     },
-    plugin: [],
-    module: {}
-}
+    plugins: [htmlWebpack],
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader']
+        }]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    }
+};
