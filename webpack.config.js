6b97690c51f69691efaf2953b5f8595a39e3f10e
karlux <lumusika@gmail.com>
2020-12-21 10:52:55 -0300

    config initial workflow

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,41 @@
 const path = require('path');
-
+const HtmlPlugin = require('html-webpack-plugin');
+const htmlplugin = new HtmlPlugin({
+    template: './assets/index.template.html',
+    filename: 'index.html'
+});
 module.exports = {
     entry: './assets/javascript/entry.js',
     output: {
         publicPath: '/',
         path: path.join(__dirname, '..'),
-        filename: 'dist/javascript/bundler.js'
+        filename: 'dist/javascript/bundle.js'
     },
-    plugin: [],
-    module: {}
+    plugins: [htmlplugin],
+    module: {
+        rules:[
+            {
+                test:/\.(c|sc|sa)ss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test:/\.(jpg|jpeg|png|gif|ico)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name:'[name].[ext]',
+                            output:'public/images'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    
+    devServer:{
+        contentBase: path.resolve(__dirname, 'dir'),
+        compress:true,
+        port: 8080
+    }
 }
